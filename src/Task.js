23a765f4c7cc2f0e961cbd000a98f7f2a8ec018c
Rick Dawson <dawsora@gmail.com>
2023-07-30 12:40:21 -0400

    converted to nodee v18

--- src/Task.js
+++ src/Task.js
@@ -1,14 +1,12 @@
-
-
 const Task = ({ text, image, index }) => {
-  return (
-    <div>
-      <h3>
-      {index + 1}. {text}{' '}
-      </h3>
-      <img style={{ maxWidth: 200 }} src={image} alt={text} />
-    </div>
-  )
-}
-
-export default Task
+    return (
+      <div>
+        <h3>
+        {index + 1}. {text}{' '}
+        </h3>
+        <img style={{ maxWidth: 200 }} src={image} alt={text} />
+      </div>
+    )
+  }
+  
+  export default Task
