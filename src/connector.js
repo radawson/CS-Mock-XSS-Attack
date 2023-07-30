a63e4ea32cacfa0bb2c79a1370064f02ec47ce6f
Rick Dawson <dawsora@gmail.com>
2023-07-30 13:10:59 -0400

    hid api key

--- src/connector.js
+++ src/connector.js
@@ -1,3 +1,5 @@
-const API_KEY = "whatevertokenyouwant"
+require('dotenv').config()
+
+const API_KEY = process.env.API_KEY
 
 const connect_string = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
