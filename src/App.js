631afd8e50c46938ca595a7e6f03429e3ad125e5
Rick Dawson <dawsora@gmail.com>
2023-07-30 12:52:24 -0400

    clean up formatting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import './App.css';
 import Task from "./Task"
 
 
@@ -18,7 +19,7 @@ const App = () => {
     image: ""
   });
 
-const handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
     if (task.text || task.image) {
       tasks.push({
@@ -26,8 +27,8 @@ const handleSubmit = e => {
         image: task.image
       })
       setTask({
-        text:"",
-        image:""
+        text: "",
+        image: ""
       });
       // Launch first attack here
     }
@@ -35,43 +36,47 @@ const handleSubmit = e => {
 
   return (
     <>
-    
-    <form onSubmit={handleSubmit}>
-      <label>
-        What do you need to get done?
-      </label>
-      <br />
-      <textarea
-                        value={task.text}
-                        onChange={e => setTask({ ...task, text: e.target.value })}
-                        defaultValue="Add a new task!"
-      />
-      <br />
-      <label>
-        Add an image to your task!
-      </label>
-      <br />
-      <input
-                        value={task.image}
-                        onChange={e => setTask({ ...task, image: e.target.value })}
-                        defaultValue="Add an image!"
-      />
-      <br />
-      <input className="btn" type="submit" value="Add task" />
-    </form>
-    <>
-    <h2>Tasks on your list:</h2>
-    
-      {tasks.map((task, index) => (
-        <Task
-        text={task.text}
-        image={task.image}
-        index={index}
-        />
-      ))}
+      <div class="box-main">
+        <form onSubmit={handleSubmit}>
+          <label>
+            What do you need to get done?
+          </label>
+          <br />
+          <textarea
+            value={task.text}
+            onChange={e => setTask({ ...task, text: e.target.value })}
+            defaultValue="Add a new task!"
+          />
+          <br />
+          <label>
+            Add an image to your task!
+          </label>
+          <br />
+          <input
+            value={task.image}
+            onChange={e => setTask({ ...task, image: e.target.value })}
+            defaultValue="Add an image!"
+          />
+          <br />
+          <input className="btn" type="submit" value="Add task" />
+        </form>
+      </div>
+      <>
+        <div class="box-main">
+          <h2>Tasks on your list:</h2>
+
+          {tasks.map((task, index) => (
+            <Task
+              text={task.text}
+              image={task.image}
+              index={index}
+            />
+          ))}
+        </div>
+      </>
+      {/* Launch second attack here. */}
     </>
-        {/* Launch second attack here. */}
-    </>
+
   )
 }
 
